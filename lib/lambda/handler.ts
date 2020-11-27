3c4bf2e83da455e1c03a0e96a7e8d3898dfc5156
Simon <simonaverhoeven@gmail.com>
2020-11-28 03:20:17 +0800

    allow cors

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -1,10 +1,23 @@
+const createResponse = (body: string, statusCode = 200) => {
+  return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'OPTIONS,GET,POST,DELETE',
+    },
+    statusCode,
+    body: JSON.stringify(body, null, 2),
+  };
+};
+
 exports.handler = async function (event: AWSLambda.APIGatewayEvent) {
   console.log('event bla: ', JSON.stringify(event, null, 2));
   //   console.log(`is prod ${process.env.isProduction}`);
 
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/plain' },
-    body: `hello from lambda your have hit ${event.path}`,
-  };
+  // return {
+  //   statusCode: 200,
+  //   headers: { 'Content-Type': 'text/plain' },
+  //   body: `hello from lambda your have hit ${event.path}`,
+  // };
+
+  return createResponse(`hello from lambda your have hit ${event.path}`);
 };
