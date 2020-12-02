05496b2ed36d148040858cd98b6d43bded41fb06
Simon <simonaverhoeven@gmail.com>
2020-12-02 10:03:40 +0800

    renamed constructs

--- lib/apigw-demo-stack.ts
+++ lib/apigw-demo-stack.ts
@@ -10,9 +10,18 @@ class ApigwDemoStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, stageName: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const baseId = `${id}-${stageName}`;
+
+    const lambdaPair = { id: `${baseId}-lambda`, description: `${baseId}-lambda` };
+    const apiPair = { id: `${baseId}-api`, description: `${baseId}-api` };
+    const stagePair = { id: `${baseId}-stage`, description: `${baseId}-stage` };
+    const deploymentPair = { id: `${baseId}-deployment`, description: `${baseId}-deployment` };
+
     // First, create a test lambda
-    const handler = new lambda.Function(this, 'lambda', {
+    const handler = new lambda.Function(this, lambdaPair.id, {
       code: lambda.Code.fromAsset(path.resolve(__dirname, 'lambda')),
+      description: lambdaPair.description,
+      functionName: lambdaPair.description,
       handler: 'index.handler',
       runtime: lambda.Runtime.NODEJS_12_X,
       environment: {},
@@ -21,15 +30,15 @@ class ApigwDemoStack extends cdk.Stack {
     // IMPORTANT: Lambda grant invoke to APIGateway
     handler.grantInvoke(new ServicePrincipal('apigateway.amazonaws.com'));
 
-    const api = new apigw.LambdaRestApi(this, `${id}-api-${stageName}`, {
-      description: `Endpoint for lambda ${stageName}`,
+    const api = new apigw.LambdaRestApi(this, apiPair.id, {
+      description: apiPair.description,
       handler,
     });
 
     // Then create an explicit Deployment construct
-    const deployment = new apigw.Deployment(this, 'my_deployment', { api });
+    const deployment = new apigw.Deployment(this, deploymentPair.id, { api, description: deploymentPair.description });
 
-    const deployedStage = new apigw.Stage(this, `${stageName}_stage`, { deployment, stageName });
+    const deployedStage = new apigw.Stage(this, stagePair.id, { deployment, stageName, description: stagePair.description });
 
     api.deploymentStage = deployedStage;
 
